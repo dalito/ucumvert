b845a766394e37477748ae5f3023681a1791ca63
David Linke <dr.david.linke@gmail.com>
2024-08-24 23:17:25 +0200

    Update to new UCUM version v2.2 (June-2024)

--- tests/test_xm_util.py
+++ tests/test_xm_util.py
@@ -2,5 +2,5 @@ from ucumvert.xml_util import get_metric_units, get_non_metric_units, get_units
 
 
 def test_get_units():
-    assert len(get_units()) == 303  # noqa: PLR2004
+    assert len(get_units()) == 305  # noqa: PLR2004
     assert set(get_units()) == set(get_metric_units() + get_non_metric_units())
